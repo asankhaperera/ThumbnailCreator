22d953ff9a1a87a800efcff025814ab900460710
asankhaperera <36698455+asankhaperera@users.noreply.github.com>
2018-03-16 18:05:07 +0530

    initial commit

--- ThumbnailCreator/lambda.js
+++ ThumbnailCreator/lambda.js
@@ -1,14 +1,42 @@
 let AWS = require('aws-sdk');
+
+// Enable ImageMagick integration.
+let gm = require('gm').subClass({ imageMagick: true });
+
+// Constants
+const MAX_WIDTH = 100;
+const MAX_HEIGHT = 100;
+
 exports.handler = function(event, context, callback) {
 
 	const response = {
-		statusCode: 200,
-		body: JSON.stringify({
-			message: 'Go SLAppForge Sigma! Your function executed successfully!',
-			input: event,
-			context: context,
-		}),
+		"isBase64Encoded": 0,
+		"statusCode": 200,
+		"headers": {
+    		"Content-Type: ": "image/png"
+  		}
 	};
+
+
+	gm(response.Body).size(function (err, size) {
+		// Infer the scaling factor to avoid stretching the image unnaturally.
+		let scalingFactor = Math.min(
+			MAX_WIDTH / size.width,
+			MAX_HEIGHT / size.height
+		);
+		let width = scalingFactor * size.width;
+		let height = scalingFactor * size.height;
+
+		// Transform the image buffer in memory.
+		this.resize(width, height)
+			.toBuffer(imageType, function (err, buffer) {
+				if (err) {
+					next(err);
+				} else {
+					next(null, response.ContentType, buffer);
+				}
+			});
+	});
 
 	callback(null,response);
 }
